d8d6220f49f3c0f3f0db6fba810b8bfb53d99bac
Vishal Jaiswal <kumardenzel@gmail.com>
2025-05-01 12:58:14 +0530

    updating versions of some librareis

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,24 +1,112 @@
+// import React, { Suspense, useEffect, useState } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+// import CanvasLoader from "../Loader";
+
+// const Computers = ({ isMobile }) => {
+//   const computer = useGLTF("./desktop_pc/scene.gltf");
+
+//   return (
+//     <mesh>
+//       <hemisphereLight intensity={0.15} groundColor='black' />
+//       <spotLight
+//         position={[-20, 50, 10]}
+//         angle={0.12}
+//         penumbra={1}
+//         intensity={1}
+//         castShadow
+//         shadow-mapSize={1024}
+//       />
+//       <pointLight intensity={1} />
+//       <primitive
+//         object={computer.scene}
+//         scale={isMobile ? 0.7 : 0.75}
+//         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+//         rotation={[-0.01, -0.2, -0.1]}
+//       />
+//     </mesh>
+//   );
+// };
+
+// const ComputersCanvas = () => {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     // Add a listener for changes to the screen size
+//     const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+//     // Set the initial value of the `isMobile` state variable
+//     setIsMobile(mediaQuery.matches);
+
+//     // Define a callback function to handle changes to the media query
+//     const handleMediaQueryChange = (event) => {
+//       setIsMobile(event.matches);
+//     };
+
+//     // Add the callback function as a listener for changes to the media query
+//     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+//     // Remove the listener when the component is unmounted
+//     return () => {
+//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
+//     };
+//   }, []);
+
+//   return (
+//     <Canvas
+//       frameloop='demand'
+//       shadows
+//       dpr={[1, 2]}
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//         />
+//         <Computers isMobile={isMobile} />
+//       </Suspense>
+
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default ComputersCanvas;
+
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
+  useEffect(() => {
+    computer.scene.traverse((child) => {
+      if (child.isMesh && child.material?.map) {
+        child.material.map.colorSpace = THREE.SRGBColorSpace;
+      }
+    });
+  }, [computer]);
+
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={4} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
+        angle={0.3}
         penumbra={1}
-        intensity={1}
+        intensity={2}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={1.5} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -33,21 +121,15 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -60,6 +142,10 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      onCreated={({ gl }) => {
+        gl.toneMapping = THREE.ACESFilmicToneMapping;
+        gl.outputEncoding = THREE.sRGBEncoding;
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -75,4 +161,8 @@ const ComputersCanvas = () => {
   );
 };
 
+// Optional: preload GLTF for performance
+useGLTF.preload("./desktop_pc/scene.gltf");
+
 export default ComputersCanvas;
+
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Increase from default 500 KB
+  }
 })
