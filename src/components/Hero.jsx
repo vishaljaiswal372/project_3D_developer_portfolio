2f9bb0b68c9df1427719d161bcc03602f175dfe0
Vishal Jaiswal <kumardenzel@gmail.com>
2025-06-27 13:51:42 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {<ComputersCanvas /> ? <ComputersCanvas />:<></>}
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
