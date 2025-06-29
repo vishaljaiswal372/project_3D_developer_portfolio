109b781538fe90971a95c6b2f79248b7372df23c
Vishal Jaiswal <kumardenzel@gmail.com>
2025-06-30 02:32:08 +0530

    update the tech because ball are not working properly in phones

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,15 +3,39 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useState , useEffect } from "react";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className='flex flex-col justify-center'>
       <h1 className='text-white text-[100px] font-bold text-center cursor-pointer'>Tech Stack</h1>
       <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          {isMobile ? <img src={technology.icon}/>:<BallCanvas icon={technology.icon}/>}
           <p className="text-center">{technology.name}</p>
         </div>
       ))}
